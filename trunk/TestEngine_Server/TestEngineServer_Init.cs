f3cea69dc8697e451d7bb764d021771b06573872
masonium <masonium@724b6b0f-0b33-0410-a495-a7cf985da2f3>
2007-08-18 08:13:51 +0000

    fixed network branch

---
+++ trunk/TestEngine_Server/TestEngineServer_Init.cs
@@ -0,0 +1,79 @@
+using System;
+using Mogre;
+using MogreNewt;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ymfas
+{
+	partial class TestEngineServer : IDisposable
+	{
+		private World world;
+		private EventManager eventMgr;
+		private YmfasServer netServer;
+
+		private Mogre.Timer frameTimer;
+
+		public enum RenderType
+		{
+			Direct3D9 = 0,
+			OpenGL = 1
+		};
+
+		public TestEngineServer(YmfasServer _server)
+		{
+			netServer = _server;
+			eventMgr = new EventManager(_server);
+		}
+
+		void Singleton_ResourceGroupLoadEnded(string groupName)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+		
+		public void PrepareGameInstance()
+		{
+			// physics system
+			InitializePhysics();            
+
+			// various other things
+			frameTimer = new Mogre.Timer();
+		}
+
+		/// <summary>
+		/// initialize the physics engine
+		/// </summary>
+		private void InitializePhysics()
+		{
+			// use faster, inexact settings
+			world = new World();
+			world.setSolverModel((int)World.SolverModelMode.SM_ADAPTIVE);
+			world.setFrictionModel((int)World.FrictionModelMode.FM_ADAPTIVE);
+            world.setWorldSize(new AxisAlignedBox(new Vector3(-WorldSizeParam), new Vector3(WorldSizeParam)));
+			world.LeaveWorld += new LeaveWorldEventHandler(OnLeaveWorld);
+		}
+
+
+		public void Dispose()
+		{
+			// destroy the server
+			if (netServer != null)
+			{
+				netServer.Dispose();
+				netServer = null;
+			}
+
+			// destroy all instance-specific information
+			if (world != null)
+			{
+				world.Dispose();
+				world = null;
+			}
+		}
+
+		public World World
+		{
+			get { return world; }
+		}
+	}
+}
